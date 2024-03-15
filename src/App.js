38a945b586766c02beb0b26283c22ad1f744d535
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-15 23:00:20 +0530

    Login as component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import React, { useState } from "react";
+import LoginForm from "./LoginForm";
 
 const App = () => {
-  const [data, setData] = useState({
-    username: "",
-    password: "",
-  });
-  const { username, password } = data;
-  const changeHandle = (e) => {
-    setData({ ...data, [e.target.name]: [e.target.value] });
-  };
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(data);
-  };
-
   return (
     <div>
       <center>
-        <form onSubmit={submitHandler}>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            placeholder="Username"
-            onChange={changeHandle}
-          />
-          <br />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Password"
-            onChange={changeHandle}
-          />
-          <br />
-          <button>Submit</button>
-        </form>
+        <LoginForm />
       </center>
     </div>
   );
