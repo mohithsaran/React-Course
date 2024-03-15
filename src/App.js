5f64327a1d1426d34fc1c457be3db8ead0396927
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-15 21:52:15 +0530

    Filter1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import React from "react";
 const App = () => {
-  const arr1 = ["React", "Vue", "Angular", "Svelte"];
-  const arr2 = [
-    {
-      id: 1,
-      title: "React",
-    },
-    {
-      id: 2,
-      title: "Vue",
-    },
-    {
-      id: 3,
-      title: "Angular",
-    },
-    {
-      id: 4,
-      title: "Svelte",
-    },
-  ];
+  const names = ["James", "John", "Paul", "George", "Washington"];
+  const filtered = names.filter((name) => name.includes("J"));
   return (
     <div>
-      {arr1.map((value, index) => (
-        <li> {value}</li>
-      ))}
-      <hr />
-      <p> From Array of objects</p>
-      {arr2.map((value, index) => (
-        <li key={value.id}> {value.title}</li>
+      {filtered.map((value) => (
+        <h4>Names containing 'J' are: {value}</h4>
       ))}
     </div>
   );
