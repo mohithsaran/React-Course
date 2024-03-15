c8004d19bee0596401a4ef9b5cc49a9c40c8724b
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-15 19:07:28 +0530

    Onclick

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import React, { useState } from "react";
-import Test from "./Test";
-
 const App = () => {
-  const [data, setData] = useState({
-    name: "Mohith",
-    age: 21,
-    skills: ["HTML ", "CSS ", "JS "],
-  });
-  const updateSkills = () => {
-    setData({ ...data, skills: ["HTML ", "CSS ", "JS ", "REACT"] });
-  };
   return (
     <div>
       <center>
-        <Test name={data.name} age={data.age} skills={data.skills} />
-        <button onClick={updateSkills}>Update Skills</button>
+        <button onClick={() => alert("Hello!")}>Click Me</button>
       </center>
     </div>
   );
