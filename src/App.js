4633aacf0d0408e01c493b2c5ba0a86afc1b9bd6
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-15 19:03:10 +0530

    Hello React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  let name = "Mohith";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h3>Hello {name}</h3>
+      </center>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Test = (props) => {
+  return (
+    <h2>
+      Name: {props.name} age: {props.age} skills:{props.skills}
+    </h2>
+  );
+};
+export default Test;
