04677624ad3ddf77dacc310d45de941bbd674e75
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-17 13:11:03 +0530

    Fetching data using axios and useEffect

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { useState, useEffect } from "react";
-// import LoginForm from "./LoginForm";
-// import Signup from "./SignupForm";
-// import Calculator from "./Calculator";
+import axios from "axios";
 
 const App = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.json())
-      .then((json) => setData(json));
-  }, []);
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setData(res.data);
+    });
+  });
   return (
     <div>
-      <center>
-        {/* <div className="Signup">
-          <Signup />
-        </div>
-        <div className="LoginForm">
-          <LoginForm />
-        </div> */}
-        {/* <Calculator /> */}
-        {data.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </center>
+      {data.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
     </div>
   );
 };
-
 export default App;
