5c96cc0aff859c3225726852cd55e2bde2bbf9f8
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-17 21:32:58 +0530

    Redirect as Navigate

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 const Home = () => {
+  const [auth, setAuth] = useState(false);
+  if (auth) {
+    return <Navigate to="/dashboard" />;
+  }
   return (
     <div>
       <h1>Welcome to Home Page</h1>
+      <button onClick={() => setAuth(true)}>Login</button>
     </div>
   );
 };
