983d860fe78346b93202bf303d5d048656a7b5d9
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-17 12:36:04 +0530

    Fetching data using Fetch API using useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import LoginForm from "./LoginForm";
 // import Signup from "./SignupForm";
-import Calculator from "./Calculator";
+// import Calculator from "./Calculator";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
   return (
     <div>
       <center>
@@ -13,7 +19,10 @@ const App = () => {
         <div className="LoginForm">
           <LoginForm />
         </div> */}
-        <Calculator />
+        {/* <Calculator /> */}
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
       </center>
     </div>
   );
